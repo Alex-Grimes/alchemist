f806d9b00d713498586634eaae821693c12b84b0
alex <alexgrimes.se@gmail.com>
2025-03-18 21:06:58 -0400

    Working through figuring out initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use axum::{Router, routing::get};
+use tracing::{Level, info};
+use tracing_subscriber;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    tracing_subscriber::fmt().with_max_level(Level::INFO).init();
+
+    dotenv().ok();
+
     println!("Hello, world!");
 }
