528cbac8858ba77db8c32c5dda19d57a97de47e0
alex <alexgrimes.se@gmail.com>
2025-03-23 13:48:36 -0400

    working template

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use axum::{
-    Extension, Json, Router, extract::Path, http::StatusCode, routing::get, serve::Listener,
+    Extension, Json, Router,
+    extract::Path,
+    http::StatusCode,
+    routing::{get, post},
 };
 use dotenvy::dotenv;
 use serde::{Deserialize, Serialize};
 use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
-use tracing::{Level, info};
+use tracing::info;
 
 #[derive(Serialize, Deserialize)]
 struct Post {
@@ -21,6 +24,31 @@ struct CreatePost {
     user_id: Option<i32>,
 }
 
+#[derive(Serialize, Deserialize)]
+struct UpdatePost {
+    title: String,
+    body: String,
+    user_id: Option<i32>,
+}
+
+#[derive(Serialize)]
+struct Message {
+    message: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct CreateUser {
+    username: String,
+    email: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: i32,
+    username: String,
+    email: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     dotenv().ok();
@@ -32,8 +60,12 @@ async fn main() -> Result<(), sqlx::Error> {
 
     let app = Router::new()
         .route("/", get(root))
-        .route("/posts", get(get_posts))
-        .route("/posts/:id", get(get_post))
+        .route("/users", post(create_user))
+        .route("/posts", get(get_posts).post(create_post))
+        .route(
+            "/posts/{id}",
+            get(get_post).put(update_post).delete(delete_post),
+        )
         .layer(Extension(pool));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
@@ -89,4 +121,55 @@ async fn create_post(
     .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     Ok(Json(post))
+}
+
+async fn update_post(
+    Extension(pool): Extension<Pool<Postgres>>,
+    Path(id): Path<i32>,
+    Json(updated_post): Json<UpdatePost>,
+) -> Result<Json<Post>, StatusCode> {
+    let post = sqlx::query_as!(Post,
+    "UPDATE posts SET title = $1, body = $2, user_id = $3 WHERE id = $4 RETURNING id, user_id, title, body",
+        updated_post.title,
+        updated_post.body,
+        updated_post.user_id,
+        id
+    )
+        .fetch_one(&pool)
+        .await;
+    match post {
+        Ok(post) => Ok(Json(post)),
+        Err(_) => Err(StatusCode::NOT_FOUND),
+    }
+}
+
+async fn delete_post(
+    Extension(pool): Extension<Pool<Postgres>>,
+    Path(id): Path<i32>,
+) -> Result<Json<serde_json::Value>, StatusCode> {
+    let result = sqlx::query!("DELETE FROM posts WHERE id = $1", id)
+        .execute(&pool)
+        .await;
+    match result {
+        Ok(_) => Ok(Json(serde_json::json! ({
+            "message": "Post deleted successfully"
+        }))),
+        Err(_) => Err(StatusCode::NOT_FOUND),
+    }
+}
+
+async fn create_user(
+    Extension(pool): Extension<Pool<Postgres>>,
+    Json(new_user): Json<CreateUser>,
+) -> Result<Json<User>, StatusCode> {
+    let user = sqlx::query_as!(
+        User,
+        "INSERT INTO users (username, email) VALUES ($1, $2) RETURNING id, username, email",
+        new_user.username,
+        new_user.email
+    )
+    .fetch_one(&pool)
+    .await
+    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    Ok(Json(user))
 }
