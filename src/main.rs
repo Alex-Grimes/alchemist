b83eff4599bb6348aa855fd39697e4d8dd517c7d
alex <alexgrimes.se@gmail.com>
2025-03-23 21:28:52 -0400

    pulled out post handelers

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+mod handlers;
 mod models;
 
 use axum::{
     Extension, Json, Router,
-    extract::Path,
     http::StatusCode,
     routing::{get, post},
 };
 use dotenvy::dotenv;
-use models::{CreatePost, CreateUser, Post, UpdatePost, User};
+use handlers::posts::{create_post, delete_post, get_post, get_posts, update_post};
+use models::{CreateUser, User};
 use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
 use tracing::info;
 
@@ -39,85 +40,6 @@ async fn main() -> Result<(), sqlx::Error> {
 
 async fn root() -> &'static str {
     "Hello, world!"
-}
-
-async fn get_posts(
-    Extension(pool): Extension<Pool<Postgres>>,
-) -> Result<Json<Vec<Post>>, StatusCode> {
-    let posts = sqlx::query_as!(Post, "SELECT id, user_id, title, body FROM posts")
-        .fetch_all(&pool)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    Ok(Json(posts))
-}
-
-async fn get_post(
-    Extension(pool): Extension<Pool<Postgres>>,
-    Path(id): Path<i32>,
-) -> Result<Json<Post>, StatusCode> {
-    let post = sqlx::query_as!(
-        Post,
-        "SELECT id, user_id, title, body FROM posts Where id = $1",
-        id
-    )
-    .fetch_one(&pool)
-    .await
-    .map_err(|_| StatusCode::NOT_FOUND)?;
-
-    Ok(Json(post))
-}
-
-async fn create_post(
-    Extension(pool): Extension<Pool<Postgres>>,
-    Json(new_post): Json<CreatePost>,
-) -> Result<Json<Post>, StatusCode> {
-    let post = sqlx::query_as!(
-    Post,
-        "INSERT INTO posts (user_id, title, body) VALUES ($1, $2, $3) RETURNING id, title, body, user_id",
-        new_post.user_id,
-        new_post.title,
-        new_post.body
-)
-        .fetch_one(&pool)
-        .await
-    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    Ok(Json(post))
-}
-
-async fn update_post(
-    Extension(pool): Extension<Pool<Postgres>>,
-    Path(id): Path<i32>,
-    Json(updated_post): Json<UpdatePost>,
-) -> Result<Json<Post>, StatusCode> {
-    let post = sqlx::query_as!(Post,
-    "UPDATE posts SET title = $1, body = $2, user_id = $3 WHERE id = $4 RETURNING id, user_id, title, body",
-        updated_post.title,
-        updated_post.body,
-        updated_post.user_id,
-        id
-    )
-        .fetch_one(&pool)
-        .await;
-    match post {
-        Ok(post) => Ok(Json(post)),
-        Err(_) => Err(StatusCode::NOT_FOUND),
-    }
-}
-
-async fn delete_post(
-    Extension(pool): Extension<Pool<Postgres>>,
-    Path(id): Path<i32>,
-) -> Result<Json<serde_json::Value>, StatusCode> {
-    let result = sqlx::query!("DELETE FROM posts WHERE id = $1", id)
-        .execute(&pool)
-        .await;
-    match result {
-        Ok(_) => Ok(Json(serde_json::json! ({
-            "message": "Post deleted successfully"
-        }))),
-        Err(_) => Err(StatusCode::NOT_FOUND),
-    }
 }
 
 async fn create_user(
